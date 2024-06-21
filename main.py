b846fcff89b34ebfa69f2ff598a68249352e3f15
Nicholas Latham <63291692+Nicklatham123@users.noreply.github.com>
2024-06-21 10:55:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 # %%
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import geopandas as gpd
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
 
